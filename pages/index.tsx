190b3d566e631a565ebe9eb04ee95456e1ab8596
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-05-01 21:02:42 +0530

    set cards as static element

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,14 @@ import HeadTag from "next/head";
 import styles from "../styles/Home.module.scss"; //Similar to .css
 import Script from "next/script";
 import Link from "next/link";
-import WhenWindowLoad from './WhenWindowLoad';
-import fs from 'fs';
+import WhenWindowLoad from "./WhenWindowLoad";
 //set Array type -> (Note:- type ka array hote hobe tar kono mane nai)
 type apiDataType = {
   id: number;
   title: string;
   paragraph: string;
   path: string;
-  slug:string;
+  slug: string;
 };
 // import StyleJsx from './Tutorial/StyleJsx'; //Style component
 //NextPage -> Home component type
@@ -27,12 +26,12 @@ const Home: NextPage = () => {
       const data = await response.json();
       setApiData(data.cardData);
     }
-    
+
     getBlogDataHome();
   }, []);
   return (
     <div className={styles.container}>
-     <WhenWindowLoad/>
+      <WhenWindowLoad />
       {/* Head tag its like Html -> <head/> (it's contain all features of that) */}
       <HeadTag>
         <title>Hunting coder</title>
@@ -55,9 +54,9 @@ const Home: NextPage = () => {
           .jsxStyle {
             color: black;
           }
-          .HomePageBlogs__card_img{
-            margin-top:2rem  !important;
-            border:2px solid red  !important;
+          .HomePageBlogs__card_img {
+            margin-top: 2rem !important;
+            border: 2px solid red !important;
           }
         `}
       </style>
@@ -96,12 +95,13 @@ const Home: NextPage = () => {
             Take a look at our most recent blog to improve <br /> your skills.
           </p>
         </header>
+        {/* Create cards as a static element because of -> Data base related (query) not currently not available */}
         <div className={styles.HomePageBlogs__body_blogs}>
-          {apiData.map((data) => (
-            <div className={styles.HomePageBlogs__body_blog} key={data.id}>
+         {/* Card ---> 1 */}
+            <div className={styles.HomePageBlogs__body_blog}>
               <div>
                 <img
-                  src={data.path}
+                  src="/HomeImg/js.png"
                   alt="lean javascript logo"
                   width={100}
                   height={100}
@@ -112,14 +112,60 @@ const Home: NextPage = () => {
                 aria-labelledby="HomePageBlog__section-heading-main"
                 className={styles.HomePageBlogs__blog_texts}
               >
-                <h2 id="HomePageBlog__section-heading-main">{data.title}</h2>
-                <p>{data.paragraph}</p>
-                <Link href={`/blogPost/${data.slug}`} passHref>
+                <h2 id="HomePageBlog__section-heading-main">How to learn JavaScript in  2022?</h2>
+                <p>We have a blog where you can learn  JavaScript.</p>
+                <Link href='/blogPost/$how-to-learn-javascript' passHref>
                   <button>Check out</button>
                 </Link>
               </section>
             </div>
-          ))}
+          {/* --------------------- */}
+           {/* Card ---> 2 */}
+           <div className={styles.HomePageBlogs__body_blog}>
+              <div>
+                <img
+                  src="/HomeImg/python.png"
+                  alt="lean javascript logo"
+                  width={100}
+                  height={100}
+                  className={styles.HomePageBlogs__card_img}
+                />
+              </div>
+              <section
+                aria-labelledby="HomePageBlog__section-heading-main"
+                className={styles.HomePageBlogs__blog_texts}
+              >
+                <h2 id="HomePageBlog__section-heading-main">How to learn Python in  2022?</h2>
+                <p>We have a blog where you can learn  python.</p>
+                <Link href='/blogPost/how-to-learn-python' passHref>
+                  <button>Check out</button>
+                </Link>
+              </section>
+            </div>
+          {/* --------------------- */}
+           {/* Card ---> 3 */}
+           <div className={styles.HomePageBlogs__body_blog}>
+              <div>
+                <img
+                  src='/HomeImg/nextJs.png'
+                  alt="lean javascript logo"
+                  width={100}
+                  height={100}
+                  className={styles.HomePageBlogs__card_img}
+                />
+              </div>
+              <section
+                aria-labelledby="HomePageBlog__section-heading-main"
+                className={styles.HomePageBlogs__blog_texts}
+              >
+                <h2 id="HomePageBlog__section-heading-main">How to learn nextJs in  2022?</h2>
+                <p>We have a blog where you can learn  nextJs.</p>
+                <Link href='/blogPost/how-to-learn-nextJs' passHref>
+                  <button>Check out</button>
+                </Link>
+              </section>
+            </div>
+          {/* --------------------- */}
         </div>
       </div>
     </div>
